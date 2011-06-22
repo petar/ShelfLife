bcc8f8daaa3fa1cefd2de1e4fcd4278e3af7ce89
Petar Maymounkov <petar@math.mit.edu>
2011-06-22 10:49:11 -0400

    added beginnings of db driver + password hmac encoding

--- src/pkg/db/db.go
+++ src/pkg/db/db.go
@@ -7,7 +7,7 @@ package db
 import (
 	"log"
 	"os"
-	//"launchpad.net/gobson"
+	gobson "launchpad.net/gobson"
 	"launchpad.net/mgo"
 )
 
@@ -34,26 +34,47 @@ func NewDb(addr string) (db *Db, err os.Error) {
 	}, nil
 }
 
-/*
-func (db *Db) AddUser(u *User) os.Error {
-	return db.c.Insert(u)
+type User struct {
+	Login        string
+	Email        string
+	HashPassword string
 }
 
-// Non-nil error indicates a connectivity problem. A missing user
-// returns u == nil and err == nil.
+func (db *Db) AddUser(u *User) os.Error {
+	return db.u.Insert(u)
+}
+
+// UserByEmail looks up a user record with the given email.
+// A non-nil error indicates a connectivity problem. 
+// A missing user returns u == nil and err == nil.
 func (db *Db) UserByEmail(email string) (u *User, err os.Error) {
 	u = &User{}
-	err = db.c.Find(gobson.M{"Email": email}).One(u)
+	err = db.u.Find(gobson.M{ "Email": email }).One(u)
 	if err == mgo.NotFound {
 		return nil, nil
 	}
 	if err != nil {
-		log.Printf("DB err: %s\n", err)
+		log.Printf("MongoDB error: %s\n", err)
 		u = nil
 	}
 	return u, err
 }
-*/
+
+// UserByLogin looks up a user record with the given login (i.e. username).
+// A non-nil error indicates a connectivity problem. 
+// A missing user returns u == nil and err == nil.
+func (db *Db) UserByLogin(login string) (u *User, err os.Error) {
+	u = &User{}
+	err = db.u.Find(gobson.M{ "Login": login }).One(u)
+	if err == mgo.NotFound {
+		return nil, nil
+	}
+	if err != nil {
+		log.Printf("MongoDB error: %s\n", err)
+		u = nil
+	}
+	return u, err
+}
 
 func (db *Db) Close() os.Error {
 	db.s.Close()
---
+++ src/pkg/db/pass.go
@@ -0,0 +1,26 @@
+// Copyright 2011 ShelfLife Authors. All rights reserved.
+// Use of this source code is governed by a
+// license that can be found in the LICENSE file.
+
+package db
+
+import (
+	"crypto/hmac"
+	"encoding/base64"
+)
+
+// passwordHMACKey is the HMAC key for the one-way transformation of plaintext passwords,
+// before they are stored in the user database. This key does not have to be secret.
+var passwordHMACKey = []byte{ 0x12, 0x13, 0x16, 0x18 }
+
+func HashPassword(password string) string {
+	sha256 := hmac.NewSHA256(passwordHMACKey)
+	sha256.Write([]byte(password))
+	hmac := sha256.Sum()
+	return textify(hmac)
+}
+
+// textify converts a byte slice into textual representation, using base64 encoding
+func textify(src []byte) string {
+	return base64.StdEncoding.EncodeToString(src)
+}
