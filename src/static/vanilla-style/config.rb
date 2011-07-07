253cbccd8adc072c4fa646bf858167659540db4d
Petar Maymounkov <petarm@gmail.com>
2011-07-07 09:37:20 -0400

    early mockup of sociability bar

--- src/static/vanilla-style/config.rb
+++ src/static/vanilla-style/config.rb
@@ -1,11 +1,14 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
+http_path = "/s/vanilla-style/"
+
 css_dir = "stylesheets"
+
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
+fonts_dir = "fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
